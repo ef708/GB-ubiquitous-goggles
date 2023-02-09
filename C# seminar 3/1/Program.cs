4a1e13b52c9c55aafe0c1a19239c2fa8bfadf91a
username <124564520+ef708@users.noreply.github.com>
2023-02-09 18:26:27 +0300

    added C# seminar 3

---
+++ C# seminar 3/1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+using System;
+
+Console.Write("Введите пятизначное число: ");
+string enteredString = Console.ReadLine();
+
+//A string reversal method
+
+string StringReversing()
+{
+
+    string reversedString = null;
+
+    for (int i = enteredString.Length - 1; i >= 0; i--)
+    {
+        reversedString = reversedString + enteredString[i];
+    }
+
+    return reversedString;
+}
+
+//Checking if the original string and reversed string match
+
+string reversedString = StringReversing();
+
+if (enteredString == reversedString)
+    Console.WriteLine($"Число {enteredString} является палиндромом");
+else
+    Console.WriteLine($"Число {enteredString} НЕ является палиндромом");
+
---
+++ C# seminar 3/2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+using System;
+
+
+Console.Write("Введите координаты x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+
+double distance = Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 0.5);
+
+Console.Write($"Расстояние между указанными точками в 3D пространстве: {distance}");
