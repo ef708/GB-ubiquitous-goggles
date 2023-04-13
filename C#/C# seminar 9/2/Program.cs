4f17fcec7132d9b2ae14ba4a37400791dfce6a3b
username <124564520+ef708@users.noreply.github.com>
2023-04-13 22:19:49 +0300

    c# seminar 9 & python seminar 7

---
+++ C#/C# seminar 9/2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+Console.Write($"Введите M и N по шаблону 'M, N': ");
+string[] input = Console.ReadLine().Split(", ");
+
+int FindThatSum(int min, int max)
+{
+    int output = min;
+    int i = min + 1;
+    while (i <= max)
+    {
+        output += i;
+        i++;
+    }
+    return output;
+}
+
+int sum = FindThatSum(int.Parse(input[0]), int.Parse(input[1]));
+Console.Write($"Сумма натуральных элементов в промежутке от {input[0]} до {input[1]}: {sum}");
---
+++ C#/C# seminar 9/3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+
+
+int Ackermann(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (m == 0)
+    {
+        return Ackermann(n - 1, 1);
+    }
+    return Ackermann(n - 1, Ackermann(n, m - 1));
+}
+
+Console.Write("Введите m и n по шаблону 'm, n': ");
+string[] input = Console.ReadLine().Split(", ");
+
+int output = Ackermann(int.Parse(input[0]), int.Parse(input[1]));
+
+Console.WriteLine(output);
